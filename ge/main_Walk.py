5d3c9d96bb6d01f295220d13341deabe93a1d4a0
sahibzada-irfanullah <sahibzada.iu@gmail.com>
2022-06-05 18:14:53 +0900

    deep walk main
    pruning deep walk main

--- ge/main_Walk.py
+++ ge/main_Walk.py
@@ -1,38 +1,20 @@
-# from struc2vec import Struc2Vec
-import networkx as nx
 from skipgram import SkipGramModel
 from DeepWalk import DeepWalk
-# from Node2Vec import Node2vec
-import random
 import os
 import networkx as nx
 import pandas as pd
 from plotting import plot_2DEmbedding
-# my_graph = nx.Graph()
-
-# Add edges to to the graph object
-# Each tuple represents an edge between two nodes
-# my_graph.add_edges_from([(10,11),(10,12),(10,13),
-#                          (11,10),(11,12),(11,13),
-#                          (12,10),(12,11),(12,13),
-#                          (13,10),(13,11),(13,12),
-#                          (14,15),(14,16),
-#                          (15,14),(15,16),
-#                          (16,14),(16,16),
-#                          (17,11),(17,13)])
-
 
 dataset = "cora"
 data_dir = "../cora"
 # dataset = "citeseer"
 # data_dir = "../citeseer/"
 data_dir = os.path.expanduser(data_dir)
-# print(os.path.join(data_dir, dataset + ".cites"))
 edgelist = pd.read_csv(os.path.join(data_dir, dataset + ".cites"), sep='\t', header=None, names=["target", "source"])
 edgelist["label"] = "cites"
 my_graph = nx.from_pandas_edgelist(edgelist, edge_attr="label")
 
-# print("my_graph", my_graph.nodes)
+
 embedDim=2         # embedding size
 numbOfWalksPerVertex = 2         # walks per vertex
 walkLength = 4
@@ -41,7 +23,6 @@ windowSize = 3
 
 dw = DeepWalk(my_graph, walkLength=walkLength, embedDim=embedDim, numbOfWalksPerVertex=numbOfWalksPerVertex, \
               windowSize=windowSize, lr = lr)
-
 
 model_skip_gram = SkipGramModel(dw.totalNodes, dw.embedDim)
 # model = dw.learnEdgeEmbedding(model_skip_gram)
