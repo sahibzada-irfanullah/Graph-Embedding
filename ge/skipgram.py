138fd18e5a328bca87de3e36d48a9c7a32b9c949
sahibzada-irfanullah <sahibzada.iu@gmail.com>
2022-06-05 19:34:29 +0900

    skipgram model fix
    skipgram model fix

--- ge/skipgram.py
+++ ge/skipgram.py
@@ -9,7 +9,7 @@ class SkipGramModel(torch.nn.Module):
         self.W1  = nn.Parameter(torch.rand((totalNodes, embedDim), requires_grad=True))
         self.W2 = nn.Parameter(torch.rand((embedDim, totalNodes), requires_grad=True))
 
-    def forward(self, one_hot):
-        hidden = torch.matmul(one_hot, self.W1)
+    def forward(self, features):
+        hidden = torch.matmul(features, self.W1)
         out    = torch.matmul(hidden, self.W2)
         return out
