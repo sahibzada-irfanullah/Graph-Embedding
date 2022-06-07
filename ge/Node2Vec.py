a1381f476911ed63cb516bedbacbe706c3fe6b53
sahibzada-irfanullah <sahibzada.iu@gmail.com>
2022-06-07 14:54:13 +0900

    RandomWalk for Node2Vec
    Developing Random walk for Node2vec

--- ge/Node2Vec.py
+++ ge/Node2Vec.py
@@ -13,3 +13,22 @@ class Node2vec(RandomWalkEmbedding):
         self.p = p
         self.q = q
 
+    def RandomWalk(self, startNode, walkLength):
+        # walk contains encoded node labels
+        walk = [int(self.nodeEncoder.transform([startNode]))]
+        walkOptions = list(self.graph[startNode])
+        if len(walkOptions)==0:
+            return walk
+        firstStep = np.random.choice(walkOptions)
+        walk.append(int(self.nodeEncoder.transform([firstStep])))
+        for k in range(walkLength-2):
+            walkOptions = list(self.graph[(self.nodeEncoder.inverse_transform([walk[-1]])[0])])
+            if len(walkOptions)==0:
+                break
+            probs = self.computeProbabilities((self.nodeEncoder.inverse_transform([walk[-2]])[0]))
+            probabilities = probs[(self.nodeEncoder.inverse_transform([walk[-2]])[0])] \
+                ['probabilities'][(self.nodeEncoder.inverse_transform([walk[-1]])[0])]
+            nextStep = np.random.choice(walkOptions, p=probabilities)
+            walk.append(int(self.nodeEncoder.transform([nextStep])))
+        return walk
+
