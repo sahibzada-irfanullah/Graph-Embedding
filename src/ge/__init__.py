507a18b27605c5d01cd08da189e1201accc90849
sahibzada-irfanullah <sahibzada.iu@gmail.com>
2022-06-16 20:11:10 +0900

    pypi
    pypi

--- src/ge/__init__.py
+++ src/ge/__init__.py
@@ -1,6 +1,7 @@
 from .utils import *
 from .skipgram import *
-from .deepWalk import *
-from .node2vec import *
-from .struc2vec import *
+from .deepWalk import DeepWalk
+from .node2vec import Node2vec
+from .struc2vec import Struc2Vec
+
 __version__ = "1.0.0"
